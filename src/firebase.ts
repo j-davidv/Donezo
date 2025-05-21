06e3961a981577332749543ffef107c4f84ccca3
j-davidv <david019villan@gmail.com>
2025-05-21 17:07:40 +0800

    Add collaboration UI improvements and debugging logs

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvBBP4BDfY_ZCt-8MPkQCIeccRA1xzWIo",
-  authDomain: "donezo-530ac.firebaseapp.com",
-  projectId: "donezo-530ac",
-  storageBucket: "donezo-530ac.firebasestorage.app",
-  messagingSenderId: "820763087623",
-  appId: "1:820763087623:web:db0d74b260267843b52ba2",
-  measurementId: "G-FNXTPZX18Q",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
